f24b7b72b6723c2b80838f5193dc81930e72aa9c
Aisha D'Souza <aishad.souza@yahoo.com>
2018-09-23 12:02:31 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+class Anagrams
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(anagram_array)
+    anagram_array.collect do |word|
+      anagram = word.split.sort
+      word = @word.split.sort
+      if anagram == word
+        return word
+      end
+    end
+
+  end
+end
